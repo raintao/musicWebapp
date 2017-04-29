6dfa4f2e51155bea4408add368c8824d85f8af23
raintao <rainlevo0819@163.com>
2017-04-29 16:20:50 +0800

    Import 导入的文件后缀省略。组件懒加载 起个名字

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import router from './router'
 import store from './store'
-import App from './App.vue'
+import App from './App'
 import Axios from 'axios'
 
 Vue.prototype.$http = Axios // 类似于vue-resource的调用方法，之后可以在实例里直接用this.$http.get()等
